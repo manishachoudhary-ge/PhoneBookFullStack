ee46941213bf8433738d9ff6448a2bce0d6f4f94
manishachoudhary-ge <manisha.choudhary@gammaedge.io>
2025-10-23 17:53:22 +0530

    Update db.js

--- Backend/src/Config/db.js
+++ Backend/src/Config/db.js
@@ -1,6 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const mongoose = require("mongoose");
-const DB_URL = "mongodb+srv://manishachoudhary_db_user:v3zCEIV62S5hKo6J@phonebook.k9gosp2.mongodb.net/?retryWrites=true&w=majority&appName=Phonebook"
-
+// const DB_URL = "mongodb+srv://manishachoudhary_db_user:v3zCEIV62S5hKo6J@phonebook.k9gosp2.mongodb.net/?retryWrites=true&w=majority&appName=Phonebook"
+const DB_URL = process.env.DB_URL;
 
 
 mongoose.connect(DB_URL)
